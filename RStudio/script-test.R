2774d95050673c8cee4e84e640d14c77c9dc9de2
dimaskeleton <69552392+dimaskeleton@users.noreply.github.com>
2024-11-16 21:41:16 -0500

    Add files via upload

---
+++ RStudio/script-test.R
@@ -0,0 +1,14 @@
+library(testthat)
+
+args <- commandArgs(trailingOnly = FALSE)
+script_path <- dirname(normalizePath(sub("--file=", "", args[grep("--file=", args)])))
+
+setwd(script_path)
+
+print(paste("Working directory:", getwd()))
+
+output_file <- "test-results.xml"
+
+results <- test_dir("tests", reporter = JunitReporter$new(file = output_file))
+
+print(paste("Tests completed, report generated at:", output_file))
